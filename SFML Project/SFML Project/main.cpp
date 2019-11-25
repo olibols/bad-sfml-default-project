f14bf632e93b69ff4f26cad8ccaf1060c0de86a8
Oliver Donnellan <olibols@aol.com>
2019-11-25 10:02:44 +0000

    g

--- SFML Project/SFML Project/main.cpp
+++ SFML Project/SFML Project/main.cpp
@@ -1,24 +1,55 @@
 #include <SFML/Graphics.hpp>
+#include <array>
+
+
+void drawSFML(sf::RenderWindow& window, sf::Drawable& drawable) {
+	window.draw(drawable);
+}
+
+struct GridTile {
+
+	GridTile() {
+		shape.setFillColor(sf::Color(255, 0, 0));
+		shape.setPosition(sf::Vector2f(1.0, 1.0));
+	};
+
+	sf::RectangleShape shape = sf::RectangleShape(sf::Vector2f(10.f, 10.f));
+};
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!"); // Makes window with dimensions 200, 200, and name "SFML works"
-	sf::CircleShape shape(100.f); // The circle that fills the whole screen lol
-	shape.setFillColor(sf::Color::Green); // Make it green
+	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
 
-	while (window.isOpen()) // The main loop that is run each frame
+	std::array<std::array<GridTile, 50>, 50>tiles;
+
+	for (int x = 0; x < 50; x++) {
+		for (int y = 0; y < 50; y++) {
+			GridTile tile;
+			tile.shape.setPosition(sf::Vector2f(x * 15, y * 15));
+
+			tiles[x][y] = tile;
+		}
+	}
+
+	while (window.isOpen())
 	{
 		sf::Event event;
-		while (window.pollEvent(event)) // Polls the window events so things like closing the window and resizing
+		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
-		window.clear(); // Clears the current frame
-		window.draw(shape); // Draws the shape to the buffer
-		window.display(); // Displays the buffer
+		window.clear(sf::Color(30, 30, 30));
+
+		for (auto& arr : tiles) {
+			for (auto& tile : arr) {
+				drawSFML(window, tile.shape);
+			}
+		}
+
+		window.display();
 	}
 
-	return 0; //  E N D
+	return 0;
 }
